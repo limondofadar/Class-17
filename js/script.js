b965fdf6d8185434d636e40f380d617b014ccfbe
Limon <115656384+Limondofader@users.noreply.github.com>
2022-11-08 11:22:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+$(function(){
+// show 
+$("#show_hide1").click(function(){
+    $(".one").toggle(400)
+})
+
+
+$("#show_hide2").click(function(){
+    $(".two").toggle(400)
+})
+
+
+$("#show_hide3").click(function(){
+    $(".three").toggle(400)
+})
+
+
+$("#show_hide4").click(function(){
+    $(".four").toggle(400)
+})
+
+
+$("#show_hide5").click(function(){
+    $(".five").toggle(400)
+})
+
+
+$("#show_hide6").click(function(){
+    $(".six").toggle(400)
+})
+
+
+$("#show_hide7").click(function(){
+    $(".seven").toggle(400)
+})
+
+
+$("#show_hide8").click(function(){
+    $(".eight").toggle(400)
+})
+
+
+$("#show_hide9").click(function(){
+    $(".nine").toggle(400)
+})
+
+
+$("#show_hide10").click(function(){
+    $(".ten").toggle(400)
+})
+
+
+
+
+
+
+
+
+})
